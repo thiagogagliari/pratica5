a042cdca60ab94c8217e3343f0f3d7116c75b2d1
Thiago Gagliari <thiagogagliari@hotmail.com>
2025-04-08 11:58:06 -0300

    Finalizando página aula 5

---
+++ script.js
@@ -0,0 +1,6 @@
+function enviar(){
+    let nome = document.getElementById('nome').value
+    let email = document.getElementById('email').value
+    let cpf = document.getElementById('cpf').value
+    let 
+}
