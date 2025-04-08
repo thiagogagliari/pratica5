d889075bd4a2f63d387d1c147e29c3b60ed1d127
Thiago Gagliari <thiagogagliari@hotmail.com>
2025-04-08 12:01:40 -0300

    Update final

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function enviar(){
-    let nome = document.getElementById('nome').value
-    let email = document.getElementById('email').value
-    let cpf = document.getElementById('cpf').value
-    let 
-}
+let nome = document.getElementById('nome').value;
+let cpf = document.getElementById('cpf').value;
+let tel = document.getElementById('tel').value;
+let senha = document.getElementById('senha').value;
